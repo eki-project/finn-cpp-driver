6e4db416fa5f130e17a6a7701d8227e2252fc1c1
Linus Jungemann <linusjun@mail.upb.de>
2023-08-17 16:39:30 +0200

    Fix linker issue

--- src/template_driver.hpp
+++ src/template_driver.hpp
@@ -2,18 +2,18 @@
 #include <string>
 #include <vector>
 
-std::string platform = "alveo";
-std::string transferMode = "memory_buffered";
+const std::string platform = "alveo";
+const std::string transferMode = "memory_buffered";
 
-std::vector<int> INPUT_BYTEWIDTH = {1, 1, 2};
-std::vector<int> OUTPUT_BYTEWIDTH = {1, 1, 2};
+const std::vector<int> INPUT_BYTEWIDTH = {1, 1, 2};
+const std::vector<int> OUTPUT_BYTEWIDTH = {1, 1, 2};
 
-std::vector<std::string> IDMA_NAMES = {"a", "b", "c"};
-std::vector<std::vector<int>> ISHAPE_NORMAL = {{1,2,3}};
-std::vector<std::vector<int>> ISHAPE_FOLDED = {{1,2,3}};
-std::vector<std::vector<int>> ISHAPE_PACKED = {{1,2,3}};
+const std::vector<std::string> IDMA_NAMES = {"a", "b", "c"};
+const std::vector<std::vector<int>> ISHAPE_NORMAL = {{1, 2, 3}};
+const std::vector<std::vector<int>> ISHAPE_FOLDED = {{1, 2, 3}};
+const std::vector<std::vector<int>> ISHAPE_PACKED = {{1, 2, 3}};
 
-std::vector<std::string> ODMA_NAMES = {"a", "b", "c"};
-std::vector<std::vector<int>> OSHAPE_NORMAL = {{1,2,3}};
-std::vector<std::vector<int>> OSHAPE_FOLDED = {{1,2,3}};
-std::vector<std::vector<int>> OSHAPE_PACKED = {{1,2,3}};
+const std::vector<std::string> ODMA_NAMES = {"a", "b", "c"};
+const std::vector<std::vector<int>> OSHAPE_NORMAL = {{1, 2, 3}};
+const std::vector<std::vector<int>> OSHAPE_FOLDED = {{1, 2, 3}};
+const std::vector<std::vector<int>> OSHAPE_PACKED = {{1, 2, 3}};

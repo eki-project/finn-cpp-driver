a327bc983149fa0e78ddf2facdf79da132f7fec8
Linus Jungemann <linus.jungemann@uni-paderborn.de>
2023-08-17 10:34:54 +0200

    Add IPO and PreventInSourceBuilds

---
+++ cmake/InterproceduralOptimization.cmake
@@ -0,0 +1,7 @@
+include(CheckIPOSupported)
+check_ipo_supported(RESULT result OUTPUT output)
+if(result)
+set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
+else()
+message(SEND_ERROR "IPO is not supported: ${output}")
+endif()

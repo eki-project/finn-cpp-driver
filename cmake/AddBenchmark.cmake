a60cd6c290930fdca74c5d98267e6898ecee79fc
Linus Jungemann <38974033+LinusJungemann@users.noreply.github.com>
2025-06-23 18:04:09 +0200

    Remove boost completely

--- cmake/AddBenchmark.cmake
+++ cmake/AddBenchmark.cmake
@@ -14,14 +14,11 @@ function(add_benchmark benchmark_name)
   target_link_libraries(${benchmark}
     PUBLIC
     finnc_options
-    ${Boost_LIBRARIES}
     finnc_utils
     finnc_core_test
     xrt_mock
     benchmark::benchmark
     OpenMP::OpenMP_CXX
   )
-
-  target_link_directories(${benchmark} PRIVATE ${BOOST_LIBRARYDIR})
 
 endfunction()

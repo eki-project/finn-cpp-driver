ed1f0a2d4872e4e4e518514e1cfd16a7c9a126f2
Linus Jungemann <38974033+LinusJungemann@users.noreply.github.com>
2025-07-10 14:44:13 +0200

    Update CI

--- benchmarks/SynchronousInferenceBenchmark.cpp
+++ benchmarks/SynchronousInferenceBenchmark.cpp
@@ -35,35 +35,6 @@ Finn::Driver<SynchronousInference> createDriverFromConfig(const std::filesystem:
     return Finn::Driver<SynchronousInference>(configFilePath, batchSize);
 }
 
-static void BM_SynchronousInference(benchmark::State& state) {
-    const std::string exampleNetworkConfig = "jetConfig.json";
-    const uint batchSize = static_cast<uint>(state.range(0));
-    auto driver = createDriverFromConfig<true>(exampleNetworkConfig, batchSize);
-    using dtype = int8_t;
-    Finn::vector<dtype> testInputs(24 * batchSize);
-
-    std::random_device rndDevice;
-    std::mt19937 mersenneEngine{rndDevice()};  // Generates random integers
-
-    destribution_t<dtype> dist{static_cast<dtype>(InputFinnType().min()), static_cast<dtype>(InputFinnType().max())};
-
-    auto gen = [&dist, &mersenneEngine]() { return dist(mersenneEngine); };
-
-    // Warmup
-    std::fill(testInputs.begin(), testInputs.end(), 1);
-    auto warmup = driver.inferSynchronous(testInputs.begin(), testInputs.end());
-    benchmark::DoNotOptimize(warmup);
-
-    std::generate(testInputs.begin(), testInputs.end(), gen);
-    for (auto _ : state) {
-        auto ret = driver.inferSynchronous(testInputs.begin(), testInputs.end());
-        benchmark::DoNotOptimize(ret);
-        benchmark::ClobberMemory();
-    }
-}
-// Register the function as a benchmark
-//BENCHMARK(BM_SynchronousInference)->Iterations(1000000)->RangeMultiplier(2)->Range(1, 4 << 10)->Repetitions(10);
-
 static void BM_SynchronousInferenceSingleThread(benchmark::State& state) {
     const std::string exampleNetworkConfig = "jetConfig.json";
     const uint batchSize = static_cast<uint>(state.range(0));

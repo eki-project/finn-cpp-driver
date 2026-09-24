4718ff95a633faa04e3f40047d55ac8329c233cd
bwintermann <bjarne.wintermann@uni-paderborn.de>
2026-09-24 13:24:57 +0200

    Bindings can be written to a custom path specified by FINN_BINDINGS_OUTPUT_DIR

--- src/FINNCppDriver/bindings/generate_stubs.sh
+++ src/FINNCppDriver/bindings/generate_stubs.sh
@@ -2,7 +2,9 @@
 # Try to generate stubs and silently handle the output. Logs are only kept if generation failed.
 LOGNAME="stubgenlog.txt"
 export PYTHONPATH="${BINDINGS_DIR}/..:$PYTHONPATH"
-cd ${BINDINGS_DIR}
+cd "${BINDINGS_DIR}/.."
+
+# BINDINGS_DIR must end with finnhpypy since this is the package name!
 ${PYBIND11_STUBGEN} -o . finnhpcpy.finnhpcpy > "$LOGNAME" 2>&1
 if [ $? -ne 0 ]; then
     echo "Stub generation failed! Check logs at $(pwd)/$LOGNAME."
